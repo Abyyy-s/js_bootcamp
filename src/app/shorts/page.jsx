9c5e02cc1585b7ee6380407033dc46b9c57d4b39
Aby S Biju <abisbiju321@gmail.com>
2026-09-24 17:41:19 +0530

    initial commit

---
+++ src/app/shorts/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+    return (
+        <div>this is my shorts page</div>
+    )
+}
+
+export default page
---
+++ src/app/trending'/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+    return (
+        <div>aby is trending now</div>
+    )
+}
+
+export default page
+import React from 'react'
+
